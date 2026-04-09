71fb28ec67e08133950fd3541152adfcbfc8d022
marcioclaudio268-code <marcioclaudio268@gmail.com>
2026-04-09 14:42:31 -0300

    cria varredura manual inicial por empresa

--- apps/api/src/modules/scans/scans.module.ts
+++ apps/api/src/modules/scans/scans.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { AuthModule } from '../auth/auth.module';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { ScansController } from './scans.controller';
+import { ScansService } from './scans.service';
+
+@Module({
+  imports: [AuthModule, PrismaModule],
+  controllers: [ScansController],
+  providers: [ScansService],
+  exports: [ScansService]
+})
 export class ScansModule {}
